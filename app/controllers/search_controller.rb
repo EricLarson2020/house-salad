3ae8e1a2adcdc66b848c6dc6c556ac776a3444b4
Eric Larson <ericlarsonbroom@gmail.com>
2020-07-02 10:55:58 -0600

    Finished Lesson

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,21 @@
+class SearchController < ApplicationController
+
+  def index
+
+    search_results = SearchResults.new
+    @members = search_results.members(params[:state])
+  #   state = params[:state]
+  #
+  #
+  #   conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+  #     faraday.headers["X-API-KEY"] = ENV['PROPUBLICA_API_KEY']
+  # end
+  #
+  #   response = conn.get("/congress/v1/members/house/#{state}/current.json")
+  #   json = JSON.parse(response.body, symbolize_names:true)
+  #   @members = json[:results].map do |member_data|
+  #     Member.new(member_data)
+  #   end
+  # end
+  end
+end
---
+++ app/poros/search_results.rb
@@ -0,0 +1,19 @@
+class SearchResults
+
+  def members(state)
+    json = PropublicaService.new.members_of_house(state)
+
+
+
+    # conn = Faraday.new(url: 'https://api.propublica.org') do |faraday|
+    #   faraday.headers['X-API-Key'] = ENV["PROPUBLICA_API_KEY"]
+    #   faraday.adapter Faraday.default_adapter
+    # end
+    #
+    # response = conn.get("/congress/v1/members/house/#{state}/current.json")
+    # json = JSON.parse(response.body, symbolize_names: true)
+    @members = json[:results].map do |member_data|
+      Member.new(member_data)
+      end
+    end
+  end
